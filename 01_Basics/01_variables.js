f0d4e246d72551586cc750e1616949c8c2c2e90c
SaradinduPal <palsaradindu03@gmail.com>
2025-09-17 19:21:11 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12345   //can't changed in future
+let accountEmail="abc@gmail.com"
+var accountPassword="11223344"
+accountCity="Jaipur"
+let accountState;   //it's only declared not value initilized 
+
+console.log(accountId);   // instead of doing console.log() for every variable we can do   console.table([write all variable name]);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";   // treat all JS code as newer version 
+
+console.log(typeof null) //null is a object type
+console.log(typeof undefined) //undefined is a undefined type
+
