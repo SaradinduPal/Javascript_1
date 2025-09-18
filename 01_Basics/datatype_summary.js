15c3e49919bd9de8e7070aa4770ab6ec1ecba43f
SaradinduPal <palsaradindu03@gmail.com>
2025-09-18 19:23:27 +0000

    Stack&Heap memory

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,27 @@
+//primitive (call by value)
+// 7 types--> String, Boolean, Number, null, undefined, Symbol, BigInt
+
+//Reference type(non-primitive) (call by reference)
+//Array, Object, Function
+
+//Array
+let arr=["Ram","Sham","Jadu"];
+
+//Object
+let obj1={
+    name:"Saradindu",
+    age:20,
+    Education:"BTech"
+}
+
+//Function 
+let fxn1= function(){
+    console.log("Hello World");
+}
+
+//-----------------------------------------------------------------------
+
+
+                //Stack And Heap Memory
+//Stack memory use in (Primitive)
+//Heap memory use in (Non-Primitive) 
