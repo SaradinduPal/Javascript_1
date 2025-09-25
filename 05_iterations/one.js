5a832ca19b4522df293a3fe1abf39660181e98a9
SaradinduPal <palsaradindu03@gmail.com>
2025-09-25 20:40:53 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,15 @@
+//for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+
+    if (element==4) {
+        console.log("4 is best number");
+    }
+    console.log(element);
+}
+
+//There also have "Nested For Loop"
+
+//There also have "break" and "continue" under the loop
+
