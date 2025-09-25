41e5006d316366fa8a62ab1eb66f20c6b0c94195
SaradinduPal <palsaradindu03@gmail.com>
2025-09-25 19:21:00 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+//SWITCH CASE
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month =3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;    
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("Default Value");
+        break;
+}
+
+//Here must write "break",if you don't write "break" then all the case {except default case} 
+//will be executed. Remember "default case" will not be executed if you don't write "break".
