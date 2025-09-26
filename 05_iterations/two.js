1412d774db77883dd4df882407a238a83881d068
SaradinduPal <palsaradindu03@gmail.com>
2025-09-26 04:34:50 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,42 @@
+//while loop
+//Syntax
+
+// while (condition) {
+    
+// }
+
+let index=0;
+while (index<=10) {
+    console.log(`index is ${index}`);
+    index +=2;
+}
+
+
+console.log("---------------------------------------------------------");
+
+
+
+//Array Printing 
+
+let nameArray=["Ram","Rohit","Riju","Rithes"]
+let i =0
+while (i < nameArray.length) {
+    console.log(`Hello ${nameArray[i]}`);
+    i+=1;
+}
+
+
+
+console.log("---------------------------------------------------------");
+
+
+
+////Do-While loop (here condition checks later)
+
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<10);
+ 
+
